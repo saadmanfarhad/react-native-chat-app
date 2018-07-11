0b66b013a391929b1033909df32be7eedefd0eab
saadmanfarhad <saadmanheavy@gmail.com>
2018-07-11 13:06:07 +0600

    Added functionalities in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,69 @@ import React from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native';
 
+import {
+  Action
+} from 'react-native-router-flux';
+
 class Home extends React.Component {
+  state = {
+    name: '',
+  }
   render() {
     return (
       <View>
-        <Text>
-          Hello Home
+        <Text style = {styles.title}>
+          Enter your name:
         </Text>
+        <TextInput
+          style = {styles.nameInput}
+          placeholder = 'Your Name'
+          onChangeText= {(text) => {
+            this.setState({
+              name: text,
+            })
+          }}
+          value = {this.state.name}
+        />
+        <TouchableOpacity
+          onPress = {( ) => {
+            //navigate and pass the name
+            Actions.chat({
+              name: this.state.name
+            });
+          }}
+        >
+          <Text style= {styles.buttonText}>
+            Next
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  title: {
+    margin: 20,
+    marginLeft: 20,
+    fontSize: 20
+  },
+  nameInput: {
+    padding: 5,
+    height: 40,
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 20
+  },
+  buttonText: {
+    marginLeft: 20,
+    fontSize: 20
+  }
+});
 
 export default Home;
